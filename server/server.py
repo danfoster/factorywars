7472780b77cb43b9a4bf1f04bb7893527a949337
Chris King <wildfir3@gmail.com>
2013-03-02 15:49:51 +0000

    basic networking - sending program card deck to clients
    Now starts up a game instance, which builds its program card deck.
    
    Also starts a listen server on port 1234 and whenever a client connects, we
    send out the deck of program cards as JSON.
    
    I've created a debug flag which controls console printouts and JSON
    minification. We might want to make this more advanced in future (e.g.
    different classes of debug messages) but thought we should get in the habit
    of handling debug output sooner rather than later

--- server/server.py
+++ server/server.py
@@ -1,21 +1,30 @@
-from entities.game import Game
-from entities.card import Card, Program
+import config
+from game import Game
+import utils
 
-programCards = []
+game = Game()
 
-for i in range(1,85):
-	if i < 7:
-		program = Program.UTurn
-	elif i < 43:
-		program = Program.RotateRight if i % 2 == 0 else Program.RotateLeft
-	elif i < 49:
-		program = Program.BackUp
-	elif i < 67:
-		program = Program.Move1
-	elif i < 79:
-		program = Program.Move2
-	else:
-		program = Program.Move3
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
 
-	programCards.append(Card(i * 10, program))
+class Client(LineReceiver):
+    def connectionMade(self):
+        if config.debug:
+            print 'new connection'
+        self.sendLine(utils.toJSON(game.programCards))
 
+    def connectionLost(self, reason):
+        if config.debug:
+            print 'lost connection'
+
+    def lineReceived(self, line):
+        if config.debug:
+            print 'received \'%s\'' % line
+
+class ClientFactory(Factory):
+    def buildProtocol(self, addr):
+        return Client()
+
+reactor.listenTCP(1234, ClientFactory())
+reactor.run()
--- server/utils.py
+++ server/utils.py
@@ -1,3 +1,5 @@
+import config
+
 # see http://stackoverflow.com/questions/36932/whats-the-best-way-to-implement-an-enum-in-python
 def enum(**enums):
     return type('Enum', (), enums)
@@ -6,7 +8,10 @@ from json import JSONEncoder
 class MyEncoder(JSONEncoder):
     def default(self, o):
         return o.__dict__  
-jsonEncoder = MyEncoder(sort_keys=True, indent=4, separators=(', ', ': '))
+jsonEncoder = MyEncoder(separators = (',', ':'))
+if config.debug:
+    jsonEncoder.separators = (', ', ': ')
+    jsonEncoder.indent = 4
 
 def toJSON(obj):
     return jsonEncoder.encode(obj)
