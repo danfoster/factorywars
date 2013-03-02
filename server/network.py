1269bd0e425935bbe67632b247008ea64a3316f9
Chris King <chris@cdlk.co.uk>
2013-03-02 20:59:55 +0000

    initial definition of server/client command enums
    and theoretical way of a client setting their nickname

---
+++ server/network.py
@@ -0,0 +1,13 @@
+from utils import *
+
+ServerCommands = enum(ProgramDeck = 0,
+                      DealProgramCards = 1,
+                      FinalPlayerChoosingOptionCards = 2,
+                      DealOptionCard = 3,
+                      DamageRobot = 4)
+
+ClientCommands = enum(MyNameIs = 0,
+                      ArrangeProgramCard = 1,
+                      ClearProgramCardArrangement = 2,
+                      PlayOptionCard = 3,
+                      PowerDownNextTurn = 4)
--- server/server.py
+++ server/server.py
@@ -1,14 +1,19 @@
 import config
 from game import Game
+from network import ClientCommands
 import utils
 
 game = Game()
 
+import json
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 
 class Client(LineReceiver):
+    def __init__(self):
+        self.nickname = '<new client>'
+
     def connectionMade(self):
         if config.debug:
             print 'new connection'
@@ -20,7 +25,12 @@ class Client(LineReceiver):
 
     def lineReceived(self, line):
         if config.debug:
-            print 'received \'%s\'' % line
+            print '<%s>: %s' % (self.nickname, line)
+
+        message = json.loads(line)
+
+        if message.command == ClientCommands.MyNameIs:
+            self.nickname = message.value
 
 class ClientFactory(Factory):
     def buildProtocol(self, addr):
