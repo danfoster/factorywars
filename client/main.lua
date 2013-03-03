214e097d34adf2d984638898e10ce7ed315588af
Adam Harwood <adam.2kah@gmail.com>
2013-03-03 18:39:47 +0000

    Fix the casing of the new font name

--- client/main.lua
+++ client/main.lua
@@ -13,7 +13,7 @@ require("states.lobbystate")
 function love.load()
     fontTitle = love.graphics.newFont("data/fonts/Spac3 halftone.ttf", 55)
     fontMenu = love.graphics.newFont("data/fonts/Typo Moiser free promo.ttf", 32)
-    fontDicks = love.graphics.newFont("data/fonts/cocksure.ttf", 45)
+    fontDicks = love.graphics.newFont("data/fonts/Cocksure.ttf", 45)
 
     MenuState.menu = Menu()
 
