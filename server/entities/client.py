d57bdc28a2b9a61c7264401f19c2c343fec3a502
Chris King <chris@cdlk.co.uk>
2013-03-03 18:18:16 +0000

    server validates client sends a card which was in its hand

--- server/entities/client.py
+++ server/entities/client.py
@@ -10,6 +10,7 @@ class Client(LineReceiver):
     def __init__(self, game):
         self.game = game
         self.nickname = '<new client>'
+        self.hand = []
         self.robot = None
 
     def connectionMade(self):
@@ -17,8 +18,8 @@ class Client(LineReceiver):
             print 'new connection'
         self.game.addClient(self)
         self.sendLine(utils.toJSON(Command(ServerCommands.ProgramDeck, self.game.deck.getDeck())))
-        hand = [self.game.deck.dealCard().id for x in range(1,10)]
-        self.sendLine(utils.toJSON(Command(ServerCommands.DealProgramCards, hand)))
+        self.hand = [self.game.deck.dealCard().id for x in range(1,10)]
+        self.sendLine(utils.toJSON(Command(ServerCommands.DealProgramCards, self.hand)))
 
     def connectionLost(self, reason):
         if config.debug:
@@ -34,7 +35,11 @@ class Client(LineReceiver):
             self.nickname = message.value
 
         elif message.command == ClientCommands.SetRegister:
-            self.robot.registers[message.value['register']] = message.value['programCardId']
+            cardId = message.value['programCardId']
+            if cardId not in self.hand:
+                raise 'Client just tried to send card \'%s\' not in their hand' % cardId
+
+            self.robot.registers[message.value['register']] = cardId
 
         elif message.command == ClientCommands.ClearRegister:
             self.robot.registers[message.value['register']] = None
