8be54145c212fa1482de5a56a8be84b1f62c039a
Adam Harwood <adam.2kah@gmail.com>
2013-03-07 01:00:50 +0000

    Pressing esc in lobby now returns to menu

--- client/states/lobbystate.lua
+++ client/states/lobbystate.lua
@@ -4,34 +4,33 @@ local Game = require("classes.game")
 local LobbyState = {}
 
 function LobbyState:init()
-    
+    -- Title
     local lblTitle = loveframes.Create("text")
     lblTitle:SetFont(fontTitle)
     lblTitle:SetText({{255, 255, 255, 255}, "Factory Wars"})
     lblTitle:CenterX()
     lblTitle:SetY(10, false)
     lblTitle:SetState("lobby")
-
+    -- Server IP label
     local lblServer = loveframes.Create("text")
     lblServer:SetFont(fontDicks)
     lblServer:SetText({{255, 255, 255, 255}, "Server IP"})
     lblServer:CenterX()
     lblServer:SetY(love.graphics.getHeight() * 1/4, true)
     lblServer:SetState("lobby")
-    
+    -- Server IP textbox
     self.txtServer = loveframes.Create("textinput")
     self.txtServer:CenterX()
     self.txtServer:SetY(love.graphics.getHeight() * 1/3, true)
-    self.txtServer:SetFocus(true)
     self.txtServer:SetState("lobby")
-    
+    -- Nickname label
     local lblNick = loveframes.Create("text")
     lblNick:SetFont(fontDicks)
     lblNick:SetText({{255, 255, 255, 255}, "Nickname"})
     lblNick:CenterX()
     lblNick:SetY(love.graphics.getHeight() * 1/2, true)
     lblNick:SetState("lobby")
-    
+    -- Nickname textbox
     self.txtNick = loveframes.Create("textinput")
     self.txtNick:CenterX()
     self.txtNick:SetY(love.graphics.getHeight()  * 6/10, true)
@@ -44,6 +43,7 @@ end
 
 function LobbyState:enter()
     loveframes.SetState("lobby")
+    self.txtServer:SetFocus(true)
 end
 
 function LobbyState:leave()
@@ -57,6 +57,8 @@ function LobbyState:keypressed(key, unicode)
             Gamestate.switch(PlayState)
             PlayState.game = Game(self.txtServer:GetText(), nil, self.txtNick:GetText())
         end
+    elseif key == "escape" then
+        Gamestate.switch(MenuState)
     end
 end
 
