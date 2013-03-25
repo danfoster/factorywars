aefa838184778b6597294959eda25ef421131e25
Adam Harwood <adam.2kah@gmail.com>
2013-03-25 23:47:03 +0000

    Added logic to execute robot movement dictated by registers

--- server/game.py
+++ server/game.py
@@ -1,5 +1,6 @@
 from entities.robot import Robot, Direction
 from entities.deck import Deck
+from entities.card import Program
 
 class Game:
 
@@ -38,9 +39,37 @@ class Game:
         robotCards = [(rob, rob.registers[regNum]) for rob in self.robots]
         # sort the list
         robotCards = sorted(robotCards, key=lambda robotCard: robotCard[1])
-
         # robots move
-        
+        for (robot, cardId) in robotCards:
+            # get card from card ID
+            card = self.deck.getCard(cardId)
+            if card.program >= 3:
+                # if card requires a tile change then check if this is possible
+                newX = robot.x
+                newY = robot.y
+                if card.program == Program.BackUp:
+                    newX, newY = robot.move(-1)
+                elif card.program == Program.Move1:
+                    newX, newY = robot.move(1)
+                elif card.program == Program.Move2:
+                    newX, newY = robot.move(2)
+                elif card.program == Program.Move3:
+                    newX, newY = robot.move(3)
+                # TODO: check that moving is legal, and find final position
+                # check for any robot collisions (pushing)
+                robot.x = newX
+                robot.y = newY
+            else:
+                # just a rotation, which is always legal
+                newO = robot.orient
+                if card.program == Program.UTurn:
+                    newO = robot.rotate(2)
+                elif card.program == Program.RotateRight:
+                    newO = robot.rotate(1)
+                elif card.program == Program.RotateLeft:
+                    newO = robot.rotate(-1)
+                robot.orient = newO
+            # TODO: send changes to clients
         # board elements move
         # lasers fire
         # touch checkpoints
